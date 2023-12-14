7d596221bc269ed9c9281a84a7d65ade6be0e1e3
Shivam <shivam18feb@gmail.com>
2023-12-14 15:26:34 +0530

    Change in vite config js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Quizer/",
+  base:"https://shivam18feb.github.io/Quizer/",
   plugins: [react()],
   
 })
